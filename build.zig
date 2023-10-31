5069bd9e33cd221c263035ec679ee8a87aa32a72
Julian Vesper <jvesper95@gmail.com>
2023-10-31 20:42:40 +0100

    Update build script to support release artifacts

--- build.zig
+++ build.zig
@@ -4,68 +4,107 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const clap = b.dependency("clap", .{
-        .target = target,
-        .optimize = optimize,
-    });
+    // === zig build (install) ===
+    {
+        const clap = b.dependency("clap", .{
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const exe_options = b.addOptions();
-    exe_options.addOption(bool, "use_gpa", b.option(bool, "use_gpa", "Use GeneralPurposeAllocator (good for debugging)") orelse (optimize == .Debug));
+        const exe_options = b.addOptions();
+        exe_options.addOption(bool, "use_gpa", b.option(bool, "use_gpa", "Use GeneralPurposeAllocator (good for debugging)") orelse (optimize == .Debug));
 
-    const exe = b.addExecutable(.{
-        .name = "comdirect-spending-calculator",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+        const exe = b.addExecutable(.{
+            .name = "comdirect-spending-calculator",
+            .root_source_file = .{ .path = "src/main.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
 
-    exe.addModule("build_options", exe_options.createModule());
-    exe.addModule("util", b.createModule(.{ .source_file = .{ .path = "src/util.zig" } }));
-    exe.addModule("clap", clap.module("clap"));
+        exe.addModule("build_options", exe_options.createModule());
+        exe.addModule("util", b.createModule(.{ .source_file = .{ .path = "src/util.zig" } }));
+        exe.addModule("clap", clap.module("clap"));
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+        // This declares intent for the executable to be installed into the
+        // standard location when the user invokes the "install" step (the default
+        // step when running `zig build`).
+        b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+        // === zig build run ===
+        {
+            const run_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+            run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+            // This allows the user to pass arguments to the application in the build
+            // command itself, like this: `zig build run -- arg1 arg2 etc`
+            if (b.args) |args| {
+                run_cmd.addArgs(args);
+            }
+
+            const run_step = b.step("run", "Run the app");
+            run_step.dependOn(&run_cmd.step);
+        }
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // === zig build test ===
+    {
+        // Creates a step for unit testing. This only builds the test executable
+        // but does not run it.
+        const unit_tests = b.addTest(.{
+            .root_source_file = .{ .path = "src/main.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+        const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_unit_tests.step);
+    }
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    // === zig build release-artifacts ===
+    {
+        const targets = [_]std.zig.CrossTarget{
+            .{ .cpu_arch = .x86_64, .os_tag = .linux },
+            .{ .cpu_arch = .x86_64, .os_tag = .windows },
+        };
+
+        const release_artifact_step = b.step("release-artifacts", "Build the release artifacts");
+
+        inline for (targets) |artifact_target| {
+            const artifact_optimize = .ReleaseSafe;
+
+            const clap = b.dependency("clap", .{
+                .target = artifact_target,
+                .optimize = artifact_optimize,
+            });
+
+            const exe_options = b.addOptions();
+            exe_options.addOption(bool, "use_gpa", false);
+
+            const artifact_exe = b.addExecutable(.{
+                .name = "comdirect-spending-calculator",
+                .root_source_file = .{ .path = "src/main.zig" },
+                .target = artifact_target,
+                .optimize = artifact_optimize,
+            });
+
+            artifact_exe.strip = true;
+
+            artifact_exe.addModule("build_options", exe_options.createModule());
+            artifact_exe.addModule("util", b.createModule(.{ .source_file = .{ .path = "src/util.zig" } }));
+            artifact_exe.addModule("clap", clap.module("clap"));
+
+            const install_artifcat_exe = b.addInstallArtifact(artifact_exe, .{
+                .dest_dir = .{
+                    .override = .{
+                        .custom = "release/",
+                    },
+                },
+            });
+
+            release_artifact_step.dependOn(&install_artifcat_exe.step);
+        }
+    }
 }
